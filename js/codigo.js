462c665edcd0f6c99787254ef8f4ef1b45876f10
Angel Munoz <612110393@utsalamanca.edu.mx>
2023-11-18 01:13:38 -0600

    Mejoras codigo 3

--- js/codigo.js
+++ js/codigo.js
@@ -19,7 +19,6 @@ function handleFileChange() {
             };
             reader.readAsDataURL(file);
         } else {
-            // Si no es una imagen, mantener la imagen predeterminada
             uploadedImage.src = '../img/enviar.png';
         }
     } else {
@@ -36,14 +35,12 @@ function encryptFile() {
     if (file) {
         const reader = new FileReader();
 
-        reader.onload = function (event) {
+        reader.onload = function(event) {
             const originalText = event.target.result;
-
-            // Proceso de encriptación
             const encryptedText = encryptText(originalText);
 
             // Almacenar el texto encriptado en sessionStorage
-            storeEncryptedText(encryptedText);
+            sessionStorage.setItem('encryptedText', encryptedText);
 
             // Redirigir a descargar.html después de encriptar
             window.location.href = 'descargar.html';
@@ -54,37 +51,11 @@ function encryptFile() {
         alert('Selecciona un archivo antes de encriptar');
     }
 }
-
-function storeEncryptedText(encryptedText) {
-    sessionStorage.setItem('encryptedText', encryptedText);
-}
-
-function encryptText(text) {
-    return CryptoJS.AES.encrypt(text, secretKey).toString();
-}
-
-function descargarArchivo() {
-    const contenidoEncriptado = sessionStorage.getItem('encryptedText');
-
-    if (contenidoEncriptado) {
-        const blob = new Blob([contenidoEncriptado], { type: 'application/octet-stream' });
-        const downloadLink = document.createElement('a');
-        downloadLink.href = URL.createObjectURL(blob);
-        downloadLink.download = 'archivo_encriptado.txt';
-
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-
-        document.body.removeChild(downloadLink);
-    } else {
-        alert('No se encontró el archivo encriptado para descargar.');
-    }
-}
-
 function decryptFile() {
     const fileInput = document.getElementById('fileInput');
     const outputTextArea = document.getElementById('output');
     const file = fileInput.files[0];
+    
 
     if (file) {
         const reader = new FileReader();
@@ -107,15 +78,53 @@ function decryptFile() {
     }
 }
 
+window.onload = function() {
+            const contenidoEncriptado = sessionStorage.getItem('encryptedText');
+            const decryptedTextArea = document.getElementById('decryptedText');
+            
+            if (contenidoEncriptado && decryptedTextArea) {
+                try {
+                    const decryptedText = decryptText(contenidoEncriptado);
+                    decryptedTextArea.value = decryptedText;
+                } catch (error) {
+                    alert('Error al desencriptar el archivo. Asegúrate de usar la clave correcta.');
+                    console.error(error);
+                }
+            } else {
+               
+            }
+        };
+
 function decryptText(encryptedText) {
-    return CryptoJS.AES.decrypt(encryptedText, secretKey).toString(CryptoJS.enc.Utf8);
+            return CryptoJS.AES.decrypt(encryptedText, secretKey).toString(CryptoJS.enc.Utf8);
+        }
+
+function encryptText(text) {
+            return CryptoJS.AES.encrypt(text, secretKey).toString();
 }
 
-window.onload = function() {
+function descargarArchivo() {
     const contenidoEncriptado = sessionStorage.getItem('encryptedText');
+
     if (contenidoEncriptado) {
-        document.getElementById('output').value = contenidoEncriptado;
+        const blob = new Blob([contenidoEncriptado], { type: 'application/octet-stream' });
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = 'archivo_encriptado.txt';
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
     } else {
-        alert('No se encontró el archivo encriptado para mostrar.');
+        alert('No se encontró el archivo encriptado para descargar.');
     }
-};
+}
+
+function storeEncryptedText(encryptedText) {
+    sessionStorage.setItem('encryptedText', encryptedText);
+}
+
+function volverIndex() {
+            window.location.href = 'index.html';
+        }
